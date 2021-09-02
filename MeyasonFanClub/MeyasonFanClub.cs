df32bf6f0204fdab49a754a289ac07e4e14a8521
yurisi0212 <Dev.yurisi@gmail.com>
2021-09-02 15:26:47 +0900

    コマンド

--- MeyasonFanClub/MeyasonFanClub.cs
+++ MeyasonFanClub/MeyasonFanClub.cs
@@ -1,9 +1,58 @@
-﻿using System;
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using System;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace MeyasonFanClub {
     class MeyasonFanClub {
+
+        private DiscordSocketClient _client;
+        private CommandService _commands;
+        private IServiceProvider _services;
+        private TokenManager _token;
+
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            new MeyasonFanClub().MainAsync().GetAwaiter().GetResult();
         }
+
+        public async Task MainAsync() {
+            _client = new DiscordSocketClient(new DiscordSocketConfig {
+                LogLevel = LogSeverity.Info
+            });
+            _client.Log += Log;
+            _commands = new CommandService();
+            _services = new ServiceCollection().BuildServiceProvider();
+            _client.MessageReceived += CommandRecieved;
+            _token = new TokenManager();
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
+            await _client.LoginAsync(TokenType.Bot, _token.Token);
+            await _client.StartAsync();
+            await Task.Delay(-1);
+        }
+
+        private async Task CommandRecieved(SocketMessage messageParam) {
+            var message = messageParam as SocketUserMessage;
+
+            if (message == null) return;
+
+            if (message.Author.IsBot) return;
+
+            var context = new CommandContext(_client, message);
+
+            var CommandContext = message.Content;
+
+            if (CommandContext == "meyason") {
+                await message.Channel.SendMessageAsync("R6S！？！？");
+            }
+        }
+
+        private Task Log(LogMessage message) {
+            Console.WriteLine(message.ToString());
+            return Task.CompletedTask;
+        }
+
     }
 }

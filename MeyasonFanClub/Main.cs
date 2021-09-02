383d5b8760c53659cff5a66f363e84ab83595b1d
yurisi0212 <Dev.yurisi@gmail.com>
2021-09-02 15:07:16 +0900

    プロジェクト ファイルを追加します。

---
+++ MeyasonFanClub/Main.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace MeyasonFanClub {
+    class Main {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ MeyasonFanClub/TokenManager.cs
@@ -0,0 +1,10 @@
+﻿namespace MeyasonFanClub {
+    public class TokenManager {
+        public TokenManager(){
+            Token = "";
+        }
+
+        public string Token { get; private set; }
+
+    }
+}
